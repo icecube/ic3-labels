a45bd7f6a594cad0997dc067c24d1362e0943496
mhuen <mhuen@users.noreply.github.com>
2019-03-03 09:58:08 +0100

    discard starting muons - test

--- ic3_labels/labels/utils/high_level.py
+++ ic3_labels/labels/utils/high_level.py
@@ -188,11 +188,12 @@ def get_muon_bundle_information(frame, convex_hull, energy_threshold=20):
         if initial_point is not None:
             # check if it is a starting muon, e.g. if intial point inside
             # is the same as the vertex
-            print(muon.pos, initial_point, (initial_point - muon.pos).magnitude)
             if (initial_point - muon.pos).magnitude > 1:
                 entry_energy = mu_utils.get_muon_energy_at_position(
                                                     frame, muon, initial_point)
                 energies_at_entry.append(entry_energy)
+            else:
+                print(muon.pos, initial_point, (initial_point - muon.pos).magnitude)
 
         cyl_energy = particle.Ei
         energies_at_cyl.append(cyl_energy)
