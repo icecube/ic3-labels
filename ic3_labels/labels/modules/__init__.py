f637d578a3023035f63066791af2f679899e437c
mhuen <mhuen@users.noreply.github.com>
2020-06-07 13:05:47 +0200

    restructure models in separate directory and import everything to __init__

---
+++ ic3_labels/labels/modules/__init__.py
@@ -0,0 +1,19 @@
+from ic3_labels.labels.modules.modules import MCLabelsDeepLearning
+from ic3_labels.labels.modules.modules import MCLabelsTau
+from ic3_labels.labels.modules.modules import MCLabelsCascadeParameters
+from ic3_labels.labels.modules.modules import MCLabelsCascades
+from ic3_labels.labels.modules.modules import MCLabelsCorsikaMultiplicity
+from ic3_labels.labels.modules.modules import MCLabelsCorsikaAzimuthExcess
+from ic3_labels.labels.modules.modules import MCLabelsMuonScattering
+from ic3_labels.labels.modules.modules import MCLabelsMuonEnergyLosses
+
+__all__ = [
+    'MCLabelsDeepLearning',
+    'MCLabelsTau',
+    'MCLabelsCascadeParameters',
+    'MCLabelsCascades',
+    'MCLabelsCorsikaMultiplicity',
+    'MCLabelsCorsikaAzimuthExcess',
+    'MCLabelsMuonScattering',
+    'MCLabelsMuonEnergyLosses',
+]
