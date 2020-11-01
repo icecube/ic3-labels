3b753e9f1f00855681456fa4e8ec011c8f2a8cf8
mhuen <mhuen@users.noreply.github.com>
2020-11-01 16:36:59 +0100

    Restructure labels for EventGeneratorMultiCascadeLabels

--- ic3_labels/labels/modules/event_generator/multi_cascade_labels.py
+++ ic3_labels/labels/modules/event_generator/multi_cascade_labels.py
@@ -3,6 +3,7 @@ from icecube import dataclasses, icetray
 
 from ic3_labels.labels.base_module import MCLabelsBase
 from ic3_labels.labels.utils import high_level as hl
+from ic3_labels.labels.utils.cascade import get_cascade_em_equivalent
 
 
 class EventGeneratorMultiCascadeLabels(MCLabelsBase):
@@ -83,19 +84,43 @@ class EventGeneratorMultiCascadeLabels(MCLabelsBase):
             extend_boundary=self._extend_boundary,
             write_mc_cascade_to_frame=False,
         )
+        primary_cascade = mc_tree.get_daughters(primary)[0]
 
         # Now extract cascade info from each daughter neutrino
         neutrinos = [p for p in mc_tree.get_daughters(primary)
                      if p.is_neutrino]
         for i, neutrino in enumerate(neutrinos):
-            labels_i = hl.get_cascade_parameters(
-                frame, neutrino,
-                convex_hull=self._convex_hull,
-                extend_boundary=self._extend_boundary,
-                write_mc_cascade_to_frame=False,
-            )
-            for key, value in labels_i.items():
-                labels[key + '_{:05d}'.format(i)] = value
+
+            cascade_name = 'cascade_{:05d}_'.format(i + 1)
+
+            daughters = mc_tree.get_daughters(neutrino)
+            if daughters[0].time > primary_cascade.time:
+                sign = 1
+            else:
+                sign = -1
+
+            distance = (
+                primary_cascade.pos - primary.daughters[0].pos
+            ).magnitude * sign
+
+            # sum up energies for daughters if not neutrinos
+            # tau can immediately decay in neutrinos which carry away energy
+            # that would not be visible, this is currently not accounted for
+            e_total, e_em, e_hadron, e_track = get_cascade_em_equivalent(
+                mctree, neutrino)
+
+            labels[cascade_name + 'x'] = daughters[0].pos.x
+            labels[cascade_name + 'y'] = daughters[0].pos.y
+            labels[cascade_name + 'z'] = daughters[0].pos.z
+            labels[cascade_name + 't'] = daughters[0].time
+            labels[cascade_name + 'distance'] = distance
+            labels[cascade_name + 'energy'] = e_total
+
+            # compute (EM equivalen) fraction of energy for each component:
+            # EM, hadronic, track
+            labels[cascade_name+'energy_fraction_em'] = e_em / e_total
+            labels[cascade_name+'energy_fraction_hadron'] = e_hadron / e_total
+            labels[cascade_name+'energy_fraction_track'] = e_track / e_total
 
         # write to frame
         frame.Put(self._output_key, labels)
