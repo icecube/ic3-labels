7e28dca9682dfe8f8b3cf08f5c62b31cd687e42d
mhuen <mhuen@users.noreply.github.com>
2021-06-18 12:05:20 +0200

    only import self veto if necessary

--- ic3_labels/weights/segments.py
+++ ic3_labels/weights/segments.py
@@ -19,7 +19,6 @@ import pickle
 
 
 from ic3_labels.weights import fluxes_corsika, fluxes_muongun, fluxes_neutrino
-from ic3_labels.weights import self_veto
 from ic3_labels.weights.mese_weights import MESEWeights
 
 
@@ -396,6 +395,8 @@ def WeightEvents(tray, name,
                        )
 
     if add_atmospheric_self_veto and dataset_type in ['nugen', 'genie']:
+        from ic3_labels.weights import self_veto
+
         tray.AddModule(
             self_veto.AtmosphericSelfVetoModule, 'AtmosphericSelfVetoModule',
             DatasetType=dataset_type,
