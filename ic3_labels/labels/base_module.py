77bdef7a277df4aae6265fd77c97a9bc412c4c6a
Mirco Huennefeld <mirco.huennefeld@tu-dortmund.de>
2023-09-26 11:05:18 +0200

    Feature/AddConvexHulls (#13)
    * Option to specify Convex Hull; Add default convex hull extensions
    
    * Feature/add default convex hulls ludwig (#14)
    
    * add any convex hull
    
    ---------
    
    Co-authored-by: mhuen <mhuen@users.noreply.github.com>
    Co-authored-by: Ludwig Neste <31670556+The-Ludwig@users.noreply.github.com>

--- ic3_labels/labels/base_module.py
+++ ic3_labels/labels/base_module.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*
 ''' I3Module Base Class
 '''
 from __future__ import print_function, division
@@ -7,6 +5,8 @@ import numpy as np
 from icecube import dataclasses, icetray
 
 from scipy.spatial import ConvexHull
+
+from ic3_labels.labels.utils import detector
 
 
 class MCLabelsBase(icetray.I3ConditionalModule):
@@ -22,6 +22,17 @@ class MCLabelsBase(icetray.I3ConditionalModule):
         self.AddParameter("MCPESeriesMapName", "Name of MCPE Series map to use.",
                           'I3MCPESeriesMap')
         self.AddParameter("PrimaryKey", "Name of the primary.", 'MCPrimary')
+        self.AddParameter(
+            "ConvexHull",
+            "The the convex hull to use. Must either be an instance of "
+            "scipy.spatial.ConvexHull or a string defining a convex hull "
+            "in ic3_labels.labels.utils.detector."
+            "Or a string named icecube_hull_ext_{extension in meters}."
+            "If None is provided, a "
+            "convex hull around the outer IceCube strings will be constructed "
+            "based on the given Geometry file.",
+            None,
+        )
         self.AddParameter("OutputKey", "Save labels to this frame key.",
                           'LabelsDeepLearning')
 
@@ -29,6 +40,7 @@ class MCLabelsBase(icetray.I3ConditionalModule):
         self._pulse_map_string = self.GetParameter("PulseMapString")
         self._mcpe_series_map_name = self.GetParameter("MCPESeriesMapName")
         self._primary_key = self.GetParameter("PrimaryKey")
+        self._convex_hull = self.GetParameter("ConvexHull")
         self._output_key = self.GetParameter("OutputKey")
 
     def Geometry(self, frame):
@@ -37,17 +49,27 @@ class MCLabelsBase(icetray.I3ConditionalModule):
                                            i[1].position.y,
                                            i[1].position.z)
                       for i in geoMap if i[1].omtype.name == 'IceCube'}
-        points = [
-            domPosDict[(31, 1)], domPosDict[(1, 1)],
-            domPosDict[(6, 1)], domPosDict[(50, 1)],
-            domPosDict[(74, 1)], domPosDict[(72, 1)],
-            domPosDict[(78, 1)], domPosDict[(75, 1)],
 
-            domPosDict[(31, 60)], domPosDict[(1, 60)],
-            domPosDict[(6, 60)], domPosDict[(50, 60)],
-            domPosDict[(74, 60)], domPosDict[(72, 60)],
-            domPosDict[(78, 60)], domPosDict[(75, 60)]
-            ]
-        self._convex_hull = ConvexHull(points)
+        if self._convex_hull is None:
+            points = [
+                domPosDict[(31, 1)], domPosDict[(1, 1)],
+                domPosDict[(6, 1)], domPosDict[(50, 1)],
+                domPosDict[(74, 1)], domPosDict[(72, 1)],
+                domPosDict[(78, 1)], domPosDict[(75, 1)],
+
+                domPosDict[(31, 60)], domPosDict[(1, 60)],
+                domPosDict[(6, 60)], domPosDict[(50, 60)],
+                domPosDict[(74, 60)], domPosDict[(72, 60)],
+                domPosDict[(78, 60)], domPosDict[(75, 60)]
+                ]
+            self._convex_hull = ConvexHull(points)
+
+        elif isinstance(self._convex_hull, str):
+            if self._convex_hull.startswith("icecube_hull_ext_"):
+                extension = float(self._convex_hull.split("_")[-1])
+                self._convex_hull = detector.get_extended_convex_hull(extension)
+            else:
+                self._convex_hull = getattr(detector, self._convex_hull)
+
         self._dom_pos_dict = domPosDict
         self.PushFrame(frame)
