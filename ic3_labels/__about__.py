3d03e08afd2b1ce2908fa31e0689293d4ffd0272
mhuen <mhuen@users.noreply.github.com>
2024-09-07 10:23:33 -0500

    Find packages automatically; bump version number for release

--- ic3_labels/__about__.py
+++ ic3_labels/__about__.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/mhuen/ic3-labels"
 
 __version_major__ = 1
 __version_minor__ = 1
-__version_patch__ = 0
-__version_info__ = "-dev"
+__version_patch__ = 1
+__version_info__ = ""
 
 __version__ = "{}.{}.{}{}".format(
     __version_major__,
