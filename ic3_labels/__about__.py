b314abd19d2f05e479c4cecf2b7ec9dce8ee89b1
mhuen <mhuen@users.noreply.github.com>
2024-09-07 10:43:53 -0500

    bump version number to development

--- ic3_labels/__about__.py
+++ ic3_labels/__about__.py
@@ -6,7 +6,7 @@ __url__ = "https://github.com/mhuen/ic3-labels"
 __version_major__ = 1
 __version_minor__ = 1
 __version_patch__ = 1
-__version_info__ = ""
+__version_info__ = "-dev"
 
 __version__ = "{}.{}.{}{}".format(
     __version_major__,
